ad5ce3325df6e5d7115f6f6e9b930f700c4b06e6
malchul <vladislaw.sorockin@yandex.ru>
2022-10-18 23:57:37 +0300

    Update logging

--- src/pipeline/face_dumper_pipeline.py
+++ src/pipeline/face_dumper_pipeline.py
@@ -1,6 +1,7 @@
 import os
 
 import cv2
+from tqdm import tqdm
 
 from src.human_keypoints_detection.mmpose_detection.human_detection.mmdet_detection import MMDetHumanDetector
 from src.human_keypoints_detection.mmpose_detection.mmpose_keypoints_detector import MMPoseDetector
@@ -12,15 +13,15 @@ from src.utils.path_utils import iterate_with_structure
 class FaceDumperPipeline:
     def __init__(self, det_config, det_ckpt, pose_config, pose_ckpt, output_size=256, device='cpu'):
         human_detector = MMDetHumanDetector(det_config=det_config, det_checkpoint=det_ckpt,
-                                      device=device)
+                                            device=device)
         self.detector = MMPoseDetector(pose_config, pose_ckpt, human_detector, device=device,
-                                  visualize=False)
+                                       visualize=False)
 
         self.pts_converter = FaceKeypoint68To4Mapper()
         self.aligner = FFHQAligner(output_size=output_size, transform_size=output_size * 4)
 
     def dump_faces(self, in_folder, out_folder):
-        for in_path, out_path in iterate_with_structure(in_folder, out_folder):
+        for in_path, out_path in tqdm(tuple(iterate_with_structure(in_folder, out_folder))):
             img = cv2.imread(str(in_path))
             img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
             _img, pose_results = self.detector.find_objects(img)
@@ -29,9 +30,8 @@ class FaceDumperPipeline:
                 face_kps = kps['face']
                 four_point = self.pts_converter.convert_points(face_kps)
 
-                out_img =  self.aligner.align(img, four_point)
+                out_img = self.aligner.align(img, four_point)
                 out_img = cv2.cvtColor(out_img, cv2.COLOR_RGB2BGR)
                 file_path, extension = os.path.splitext(str(out_path))
-                file_path = file_path + f'_{crop_id:2d}' +  extension
+                file_path = file_path + f'_{crop_id:2d}' + extension
                 cv2.imwrite(file_path, out_img)
-
